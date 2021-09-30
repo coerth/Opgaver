0b9232ab2fc72fa9188c040980157d57e1a49303
Morten <59032722+coerth@users.noreply.github.com>
2021-09-30 13:54:35 +0200

    30-09-21-Torsdagsopgaver

---
+++ 30-09-21-Torsdag/src/com/company/Letteroccurence.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+public class Letteroccurence implements Comparable<Letteroccurence> { //skal adde implements Comparable<Object> for at kunne sortere arraylist
+
+
+    private char theCharacter;
+    private int numberOfOccurences;
+
+    public Letteroccurence(char theCharacter, int numberOfOccurences) {
+        this.theCharacter = theCharacter;
+        this.numberOfOccurences = numberOfOccurences;
+    }
+
+    public char getTheCharacter() {
+        return theCharacter;
+    }
+
+    public int getNumberOfOccurences() {
+        return numberOfOccurences;
+    }
+
+
+   // @Override
+    public int compareTo(Letteroccurence otherLetter) { //skal lave en metode som hedder compareTo for at kunne lave sortering, på den værdi du vil have
+        return this.numberOfOccurences - otherLetter.numberOfOccurences;
+    }
+}
---
+++ 30-09-21-Torsdag/src/com/company/Main.java
@@ -0,0 +1,211 @@
+package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.*;
+
+public class Main
+{
+    static Scanner scan;
+    private static String[] text;
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File file = new File("data.txt");
+        scan = new Scanner(file);
+
+        String inputFromFile = "";
+
+        while (scan.hasNextLine())                                // checks if theres more lines in the file
+        {
+            inputFromFile += scan.nextLine();                     // adds each line to the inputFromFile string.
+        }
+
+        text = inputFromFile.split(" ");                    // Creates and array of strings, where each element is a single word from the file.
+       // System.out.println(text.length);
+
+        //printWordsStartingWith("Ø");
+
+        //printWordsOfLength(3);
+
+
+        //test dine metoder ved at kalde dem her:
+        //System.out.println(printLongestWord());
+        //printFirstHalfOfEachWord();
+        printMostFrequentLetter();
+        printLessFrequentLetter();
+
+
+    }
+
+    private static void printWordsOfLength(int l)
+    {
+        boolean wordisvalid = true;
+
+        for (String s : text)
+        {
+            if (s.length() == l)
+            {
+                if (s.contains(",") || s.contains("."))
+                {
+                    wordisvalid = false;
+                }
+
+                if (wordisvalid)
+                {
+                    System.out.println(s);
+                }
+            }
+        }
+    }
+
+    private static void printWordsStartingWith(String pattern)
+    {
+        for (String s : text) // for each word in text
+        {
+            if (s.startsWith(pattern) || s.startsWith(pattern.toLowerCase()))
+            {
+                System.out.println(s);
+            }
+        }
+    }
+
+    //skriv dine metoder herunder:
+    //Task 1 funktion
+    private static String printLongestWord()
+    {
+        int lengthCounter = 0;
+        int index;
+        String longestWord = "";
+
+
+        for (String s : text)
+        {       //hvis ordets længde er større end længdecounteren, så gør følgende:
+                if(s.length() > lengthCounter){
+                     longestWord = s;
+                    lengthCounter = longestWord.length();
+
+                    //hvis longestWord indeholder et . så gør følgende:
+                    if (longestWord.contains(".")){
+                        index = longestWord.indexOf(".");
+                        longestWord = longestWord.substring(0, index );
+                        lengthCounter = longestWord.length();
+                    }
+
+                    //eller hvis longestWord indeholder et , så gør følgende:
+                    else if (longestWord.contains(",")){
+                        index = longestWord.indexOf(",");
+                        longestWord = longestWord.substring(0, index );
+                        lengthCounter = longestWord.length();
+                    }
+        }
+            }
+        return longestWord;
+        }
+
+        //Task 2
+    public static void printFirstHalfOfEachWord() {
+        for (String s : text) {
+            System.out.println(s.substring(0, s.length() / 2));
+            //printer også hele ordet for at se om den tog mellemrum eller det var tomme værdier i arrayet
+            System.out.println(s);
+        }
+        System.out.println("\n");
+    }
+
+    //Task 3
+    public static void printMostFrequentLetter(){
+        ArrayList<Letteroccurence> letterArray = new ArrayList<>(); //array til at sortere en top 10
+        int charCounter = 0; // counter brugt for hvert bogstav
+        String checkText = ""; //nem løsning for at tjekke hele string arrayet
+        int sortCounter = 0; // for at kunne bruges i en if betingelse til printningen af vores top 10
+
+        //for hvert ord i String arrayet, læg det ord til vores checkText string
+        for (String s : text) {
+            checkText += s.toLowerCase(Locale.ROOT); // Gør alle bogstaver til lowercase
+        }
+            //For loop som kører igennem hvert bogstav i alfabetet og kører endnu en for loop som tjekker vores checkText
+            // for det bogstav vi er nået til og adder til counteren når vi når dertil. Den tager ikke æ ø og å,
+            // men har lavet en føler på at det ikke er en af de bogstaver der forekommer hyppigst.
+            for(char letter = 'a'; letter <= 'z'; letter++ ){
+
+            for(int i = 0; i < checkText.length(); i++){
+                if(checkText.charAt(i) == letter){
+                    charCounter++;
+                    //System.out.println(s);
+                }
+            }
+            //når tjekket efter et specifikt bogstav er færdigt og der er fundet værdi på counteren over 0,
+                // print bogstavet + counter ud
+
+            if(charCounter > 0){
+                letterArray.add(new Letteroccurence(letter,charCounter) );
+                //System.out.println(letter + "=" + charCounter);
+                charCounter = 0;
+            }
+                sortCounter++; // når vi er færdige med hvert bogstav så går sortCounter en op.
+            if(sortCounter == 26){ //når vi er nået til det 26. bogstav så gør følgende:
+                Collections.sort(letterArray); //sorter vores array, den største værdi er den sidste i arrayet
+                System.out.println("Top 10 for størst hyppighed. Æ,Ø,Å er ikke inkluderet");
+
+
+                // Da den største værdi er den sidste så start fra slutningen af arrayet og bevæg dig tilbage
+                for(int i = letterArray.size()-1; i > letterArray.size()-11 ; i--){
+                    System.out.println(letterArray.get(i).getTheCharacter() + " = " + letterArray.get(i).getNumberOfOccurences());
+                }
+            }
+            }
+        System.out.println("\n");
+    }
+
+    //Task 4
+    public static void printLessFrequentLetter(){
+        ArrayList<Letteroccurence> letterArrayLess = new ArrayList<>(); //array til top 10 af de færreste, behøves ikke at lave navnet om men det blev gjort
+        int charCounter = 0; // counter brugt for hvert bogstav
+        String checkText = ""; //nem løsning for at tjekke hele string arrayet
+        int sortCounter = 0; // for at kunne bruges i en if betingelse til printningen af vores top 10
+        //for hvert ord i String arrayet, læg det ord til vores checkText string
+        for (String s : text) {
+            checkText += s.toLowerCase(Locale.ROOT); // Gør alle bogstaver til lowercase
+        }
+        //for loop som kører igennem hvert bogstav i alfabetet og kører endnu en for loop som tjekker vores checkText
+        // for det bogstav vi er nået til og adder til counteren når vi når dertil. Den tager ikke æ ø og å,
+        // men har lavet en føler på at det ikke er en af de bogstaver der forekommer hyppigst.
+        for(char letter = 'a'; letter <= 'z'; letter++ ){
+
+            for(int i = 0; i < checkText.length(); i++){
+                if(checkText.charAt(i) == letter){
+                    charCounter++;
+                    //System.out.println(s);
+                }
+            }
+            //når tjekket efter et specifikt bogstav er færdigt og der er fundet værdi på counteren over 0,
+            // print bogstavet + counter ud
+
+            if(charCounter > 0){
+                letterArrayLess.add(new Letteroccurence(letter,charCounter) );
+                //System.out.println(letter + "=" + charCounter);
+                charCounter = 0;
+            }
+            sortCounter++; // når vi er færdige med hvert bogstav så går sortCounter en op.
+            if(sortCounter == 26){ //når vi er nået til det 26. bogstav så gør følgende:
+                Collections.sort(letterArrayLess); //sorter vores array, den mindste værdi er den første i arrayet
+                System.out.println("Top 10 for mindst hyppighed. Æ,Ø,Å er ikke inkluderet");
+
+                // Da den mindste værdi er den første så start fra starten af arrayet og bevæg dig frem
+                for(int i = 0; i< 10; i++){
+                    System.out.println(letterArrayLess.get(i).getTheCharacter() + " = " + letterArrayLess.get(i).getNumberOfOccurences());
+                }
+            }
+        }
+        System.out.println("\n");
+    }
+}
+
+
+
+
+
