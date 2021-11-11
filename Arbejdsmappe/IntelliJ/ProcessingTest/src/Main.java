6db1700ee1def3ee28499e1e47f65270c8a27134
coerth <59032722+coerth@users.noreply.github.com>
2021-11-11 20:27:51 +0100

    processing halløj

---
+++ Arbejdsmappe/IntelliJ/ProcessingTest/src/Main.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+import processing.core.PFont;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        ProcessingTest processingTest = new ProcessingTest();
+        PApplet.main("Main", args);
+
+    }
+
+}
+
+
